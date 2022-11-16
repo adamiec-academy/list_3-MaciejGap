b1128784825806b024af202109c8ae086b5d9352
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-11-16 15:42:02 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,27 @@
-# Remove this comment to confirm that this task is done
+from turtle import *
+import random
+
+
+def mountain_height_change():
+    return random.randint(5, 40)
+
+
+def mountain_length_change():
+    return random.randint(3, 30)
+
+
+def mountains(length, height, mountain_range):
+
+    for i in range(mountain_range):
+        goto(length * i, mountain_height_change() + i * random.randint(5, 20))
+
+    for i in range(mountain_range):
+        goto(length * i + mountain_range * length, mountain_height_change() + (mountain_range - i) * random.randint(5, 20))
+
+
+speed("slowest")
+
+mountains(10, 50, 12)
+
+
+exitonclick()
