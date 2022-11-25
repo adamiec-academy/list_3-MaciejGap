98bd3b6098f3bdd17c0db14a39e8847c720efd9d
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-11-25 13:09:35 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,1 +1,24 @@
-# Remove this comment to confirm that this task is done
+from turtle import *
+
+
+def star():
+
+    tracer(0, 1)
+
+    update()
+
+    for arm in range(10):
+
+        for j in range(20):
+            forward(10 + 3 * j)
+            penup()
+            backward(10 + 3 * j)
+            right(360 / 10)
+            forward(5)
+            left(80)
+            pendown()
+
+    exitonclick()
+
+
+star()
