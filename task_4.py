bbada21e7a982c420fe838b9ff4f5a7281ffd103
MaciejGap <115427042+MaciejGap@users.noreply.github.com>
2022-11-25 13:54:12 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,5 +1,12 @@
 def tower(n):
-    pass
+
+    total_width = 2 * n - n
+
+    for i in range(1, n + 1):
+        current_width = 2 * i - 1
+        for _ in range(3):
+
+            print((total_width - 1 * i) * " " + (2 * i - 1) * "#" + (total_width - 1 * i) * " ")
 
 
 def towers(data):
